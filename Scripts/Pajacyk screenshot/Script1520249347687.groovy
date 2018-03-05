cd295caefdebe5516ca0ab5c8865692f11e3c9d9
bulinskim <bulinskim@CKHDHM2.gbcc.local>
2018-03-05 12:32:15 +0100

    Pajacyk screenshot

---
+++ Scripts/Pajacyk screenshot/Script1520249347687.groovy
@@ -0,0 +1,50 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+String napis = GlobalVariable.intro + GlobalVariable.licznik+ GlobalVariable.outro;
+
+WebUI.navigateToUrl('https://www.pajacyk.pl/#index')
+
+WebUI.comment('licznik: '+GlobalVariable.licznik.toString())
+
+WebUI.comment('sciezka: '+ napis)
+
+WebUI.takeScreenshot(napis)
+
+WebUI.click(findTestObject('Page_Pajacyk  Pajacyk od wielu lat/div_paj-click'))
+
+GlobalVariable.licznik=GlobalVariable.licznik+1;
+
+napis = GlobalVariable.intro + GlobalVariable.licznik+ GlobalVariable.outro;
+
+WebUI.comment('licznik: '+GlobalVariable.licznik.toString())
+
+WebUI.comment('sciezka: '+ napis)
+
+WebUI.takeScreenshot(napis)
+
+WebUI.closeBrowser()
+
+
+
