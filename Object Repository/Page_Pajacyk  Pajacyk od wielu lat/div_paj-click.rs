117a119057b988b0b5c96b6fdecff6732d538569
bulinskim <bulinskim@CKHDHM2.gbcc.local>
2018-03-05 12:20:13 +0100

    Pajcyk test case

---
+++ Object Repository/Page_Pajacyk  Pajacyk od wielu lat/div_paj-click.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_paj-click</name>
+   <tag></tag>
+   <elementGuidId>faf0b85b-f182-4742-be4c-2957af18471c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>paj-click</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;index&quot;)/div[@class=&quot;pajac&quot;]/div[@class=&quot;paj-click&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
